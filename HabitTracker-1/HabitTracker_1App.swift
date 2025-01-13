875e2ec9f791b6524a898f8e326733a0ae7bd1db
sahinkarahan <sahin.karahan@hotmail.com>
2025-01-13 08:20:32 +0300

    Initial Commit

---
+++ HabitTracker-1/HabitTracker_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTracker_1App.swift
+//  HabitTracker-1
+//
+//  Created by Şahin Karahan on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTracker_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
